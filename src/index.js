83acea13a298b2ab04b57c0614016fa2b41bf913
indrajeetsuryawanshi <indrajeetsuryawanshi2592@gmail.com>
2022-08-14 12:17:57 +0530

    missing number s=assignment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
-
+const{default: mongoose}=require('mongoose')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb+srv://indrajeetsuryawanshi:kHo53Tt2sY3LaEpW@cluster0.lmzpn5e.mongodb.net/?retryWrites=true&w=majority"
+,{
+    useNewUrlParser:true
+}
+).then(()=>{console.log("MongoDb is connet")})
+.catch(err=>console.log(err))
 
 app.use('/', route);
 
