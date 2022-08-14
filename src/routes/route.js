72039b99e2d6d8227ce3c2c0cf592b686ecaef0e
indrajeetsuryawanshi <indrajeetsuryawanshi2592@gmail.com>
2022-08-14 17:13:42 +0530

    mongodb session 1

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,7 +6,7 @@ const UserController= require("../controllers/userController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
+    
 router.post("/createUser", UserController.createUser  )
 
 router.get("/getUsersData", UserController.getUsersData)
