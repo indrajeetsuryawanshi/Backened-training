9fafc405cd4e6d252b9127faba54e8371420b2ee
indrajeetsuryawanshi <indrajeetsuryawanshi2592@gmail.com>
2022-08-15 20:21:44 +0530

    assign on players&voting

--- src/routes/route.js
+++ src/routes/route.js
@@ -66,47 +66,47 @@ const router = express.Router();
 //     let ele= req.body.element
 //     arr.push(ele)
 //     res.send(  { msg: arr , status: true }  )
-// })
-// let players=
-// [{ 
-//     "name": "indrajeet",
-//     "dob" : "13/02/2000",
-//      'age': "23",
-//      "gender": "male"
-// } ,{
-//     "name": "sanket",
-//     "dob" : "28/07/2000",
-//      'age': "22",
-//      "gender": "male"
-// },{
-//      "name": "saurav",
-//      "dob" : "28/4/2000",
-//       'age': "22",
-//       "gender": "male"
+
+let players=
+[{ 
+    "name": "indrajeet",
+    "dob" : "13/02/2000",
+     'age': "23",
+     "gender": "male"
+} ,{
+    "name": "sanket",
+    "dob" : "28/07/2000",
+     'age': "22",
+     "gender": "male"
+},{
+     "name": "saurav",
+     "dob" : "28/4/2000",
+      'age': "22",
+      "gender": "male"
 
 
-// }]
-// router.post("/players", function(req, res) {
-//     // let players=[]
-//     let newplayer=req.body
-//     let nameofnewplayer =newplayer.name
-//     let isreapeatedname= false
-// for (let i=0;i<players.length;i++){
-//     if(players[i].name==nameofnewplayer){
-//     isreapeatedname = true;
-//    break;
-// }
-// }
-//      if (isreapeatedname){
-//     res.send("the name is already exist")
-// }else{
-// players.push(newplayer)
-// res.send( {data:players})
-// } 
-// })
+}]
+router.post("/players", function(req, res) {
+    // let players=[]
+    let newplayer=req.body
+    let nameofnewplayer =newplayer.name
+    let isreapeatedname= false
+for (let i=0;i<players.length;i++){
+    if(players[i].name==nameofnewplayer){
+    isreapeatedname = true;
+   break;
+}
+}
+     if (isreapeatedname){
+    res.send("the name is already exist")
+}else{
+players.push(newplayer)
+res.send( {data:players})
+} 
+})
 
 
-    let person =[{
+    let personarr =[{
         name:"pk",
         age:10,
         votingstatus:false},
@@ -125,38 +125,25 @@ const router = express.Router();
         age:45,
         votingstatus:false
         }]
-        router.post("/votingstatusperson", function(req, res){
-let votingage= req.query.age;
-let eligibleperson=[]
-for (let i=0;i<person.length;i++){
-     if(person[i].age> votingage){
-      person[i].votingstatus=true;
-        eligibleperson.push(person[i])
-    }
-  res.send({data:eligibleperson, status:true})
+        router.post('/voting-post',function(req,res){
+                let voting=req.query.input;
+                let finalarr=[]
+                for(let i=0;i<personarr.length;i++){
+            
+                    let list=personarr[i]
+                    if (list.age>=voting){
+                       list.votingstatus=true
+            
+                       finalarr.push(personarr[i])
+                      
+                    }
+                }   
+              res.send({data:finalarr,status:true})
+            })
 
-}
-    
-});
 
 
-
-// router.post('/voting-post',function(req,res){
-//     let querypar=req.query.input;
-//     let finalarr=[]
-//     for(let i=0;i<peopleList.length;i++){
-
-//         let list=peopleList[i]
-//         if (list.age>=querypar){
-//            list.votingstatus=true
-
-//            finalarr.push(peopleList[i])
-          
-//         }
-//     }   
-//   res.send({data:finalarr,status:true})
-// })
-
+//
 
 
 
