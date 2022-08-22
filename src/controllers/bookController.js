3d9f4b2c6226390b7af8f99d73eabd0d07249880
indrajeetsuryawanshi <indrajeetsuryawanshi2592@gmail.com>
2022-08-22 22:17:09 +0530

    assign on refernce and populate

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -25,13 +25,13 @@ const getbookwithauthorpublisher=async function(req ,res){
 res.send({data: getbook})
 }
 const Newupdatebook= async function(req,res){
-// let publisher= await PublisherModel.find({$or:[{name:"Penguin"},{name:"harpercollins"}]}).select({_id:1})
-// let updatebook = await BookModel.updateMany({publisher_id:publisher},{$set:{ishardcover:true}})
+    let publisher= await PublisherModel.find({$or:[{name:"penguin"},{name:"harpercollins"}]}).select({_id:1})
+    let updatebook= await BookModel.updateMany({publisher_id:publisher},{$set:{ishardcover:true}})
 let author= await AuthorModel.find({rating:{$gt:3.5}}).select({_id:1})
 let updateprice= await BookModel.updateMany({author_id:author},{$inc:{price:10}})
-let updateboo= await BookModel.find().populate(['author_id', 'publisher_id'])
-
-res.send({data:updateboo})
+// let updateboo= await BookModel.find().populate(['author_id', 'publisher_id'])
+// res.send({data:updateboo})
+    return res.send({msg:updatebook,updateprice})
 }
 
 // res.send({data: publisher})
@@ -44,17 +44,5 @@ module.exports.Newupdatebook=Newupdatebook
 
 
 
-// createBook
-// onst deleteBooks= async function (req, res) {
-//     // let data = req.body 
-//     let allBooks= await BookModel.updateMany( 
-//         { authorName: "FI"} , //condition
-//         { $set: {isDeleted: true} }, //update in data
-//         { new: true } ,
-
-// var id = req.params.id;
-//     collection.update(
-//     {_id: id}, {
-//         $set: item
 
 
