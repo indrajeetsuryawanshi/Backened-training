84f568552871cc93da93b3b713b9a22c1f60acba
indrajeetsuryawanshi <indrajeetsuryawanshi2592@gmail.com>
2022-08-27 17:27:00 +0530

    assignment on middleware session 1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment=require('moment')
 const newmid=  require('./middlewares/commonMiddlewares');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://indrajeetsuryawanshi:kHo53Tt2sY3LaEpW@cluster0.lmzpn5e.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -17,10 +18,11 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 
 app.use (function (req, res,next){
-    let currenttime=new Date();
+    const today=moment().format('YYYY-MM-DD')
+  
 let ip = req.ip
 let path = req.path
-console.log (currenttime,ip,path)
+console.log (today,ip,path)
 
 
 next();
