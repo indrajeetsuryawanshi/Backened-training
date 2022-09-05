643c4674fdd9469f3ed4fbec4ebf0e0024553b6e
Love2Rizwan <love2rizwan@gmail.com>
2022-09-05 17:58:13 +0530

    Create-Package.json

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const bodyParser = require('body-parser');
+const route = require('./route/route.js');
+const app = express();
+
+
+// use Nodejs bodyParser middleware
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/ x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Database connected
+mongoose.connect("mongodb+srv://prabhas:password123*@cluster0.j1kcoh4.mongodb.net/group57Database", {
+          useNewUrlParser: true
+
+}).then(() => console.log("MongoDB Connected"))
+          .catch(err => console.log(err));
+
+
+
+// define route
+app.use('/', route);
+
+
+// Application listeners
+app.listen(process.env.PORT || 3000, function () {
+          console.log("Server running on PORT  " + (process.env.PORT || 3000));
+});
