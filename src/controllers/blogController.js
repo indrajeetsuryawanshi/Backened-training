35f733242e5a5c2d4e1b013dd56bcdff02e496b9
debasish-afk <debasishbehera904@gmail.com>
2022-09-06 14:47:30 +0530

    createBlog

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,3 +1,19 @@
-const BlogModel= require('/..models/blogModel')
+const BlogModel = require('../models/blogsModel')
+const AuthorModel = require('../models/authorModel')
 
 
+const createBlog = async function (req, res) {
+    try {
+        let  data= req.body
+         let authorid=await AuthorModel.findById(data.authorId)
+        if (!authorid)
+            return res.status(404).send({ msg: "author_id is not valid" })
+        let createBlogs = await BlogModel.create(data)
+        res.status(201).send({ msg: createBlogs })
+    }
+    catch (err) {
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+module.exports.createBlog = createBlog
